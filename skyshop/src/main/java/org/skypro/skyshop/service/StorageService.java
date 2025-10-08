4843a5dbae3c8f4b156acf8f1a93ac32bd147627
homework <kirill_gavrilov01@bk.ru>
2025-10-08 21:55:21 +1000

    homework

--- skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
+++ skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -1,61 +1,19 @@
 package org.skypro.skyshop.service;
 
+// Импортируйте нужные типы
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.Searchable;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public interface StorageService {
+    Collection<Searchable> getAllSearchables(); // Метод для получения всех поисковых объектов
 
     Collection<Product> getAllProducts();
 
+    Collection<Article> findArticlesByQuery(String query);
+
+    Collection<Product> findProductsByQuery(String query);
+
     Collection<Article> getAllArticles();
-
-    List<Product> findProductsByQuery(String query); // Новый метод поиска продуктов
-
-    List<Article> findArticlesByQuery(String query); // Новый метод поиска статей
-
-    Collection<Searchable> getAllSearchables();
-}
-
-// Класс реализации интерфейса StorageServiceImpl.java
-class StorageServiceImpl implements StorageService {
-
-    // Предположим, у вас есть хранилище данных
-    private Collection<Product> products;
-    private Collection<Article> articles;
-
-    @Override
-    public Collection<Product> getAllProducts() {
-        return List.of();
-    }
-
-    @Override
-    public Collection<Article> getAllArticles() {
-        return List.of();
-    }
-
-    // Примеры реализации методов поиска
-    @Override
-    public List<Product> findProductsByQuery(String query) {
-        return products.stream()
-                .filter(product -> product.getName().contains(query))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Article> findArticlesByQuery(String query) {
-        return articles.stream()
-                .filter(article -> article.getTitle().contains(query))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Collection<Searchable> getAllSearchables() {
-        return List.of();
-    }
-
-    // Другие методы...
 }
